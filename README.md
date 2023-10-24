b121e35388e07216801b8829429afe6150be109a
kaoplo <kaoplo@pawz.hu>
2023-10-24 23:22:14 +0200

    created the new version for the app, fully funcional

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # TwitchPyTTS
-more info soon
+Powered by PyTwitchAPI
+
+## Dependencies
+Make sure you run these in a venv
+
+Install the required packages
+```bash
+pip install twitchapi pyttsx3
+```
+
+Install the required packages on your system (needed by pyttsx3)
+```bash
+ sudo apt update && sudo apt install espeak ffmpeg libespeak1
+```
+
+## Before running
+Create an application on dev.twitch.tv
+
+Edit the `config.json` file
+```json
+{
+  "AppID": "",
+  "AppSecret": "",
+  "Channel": ""
+}
+```
+
+## Running the application
+While in the venv, run `main.py`
+```bash
+python main.py
+```
--- main.py
+++ main.py
@@ -1,21 +1,55 @@
 import pyttsx3
-from twitch_chat_irc import twitch_chat_irc
+from twitchAPI.twitch import Twitch
+from twitchAPI.oauth import UserAuthenticator
+from twitchAPI.type import AuthScope, ChatEvent
+from twitchAPI.chat import Chat, EventData, ChatMessage
+import asyncio
+import json
+
+with open('config.json', 'r') as f:
+    config = json.load(f)
+
+APP_ID = config['AppID']
+APP_SECRET = config['AppSecret']
+USER_SCOPE = [AuthScope.CHAT_READ, AuthScope.CHAT_EDIT]
+TARGET_CHANNEL = config['Channel']
 
 
 def speak_message(message):
-    toSay = message['display-name'] + ": " + message['message']
-    print(toSay)
-    print(message)
+    print("saying: " + message)
     engine = pyttsx3.init()
-    engine.say(toSay)
+    engine.say(message)
     engine.runAndWait()
 
 
-
-channel = "kaoplo"
-
-connection = twitch_chat_irc.TwitchChatIRC()
-
-messages = connection.listen(channel, on_message=speak_message)
+async def on_ready(ready_event: EventData):
+    await ready_event.chat.join_room(TARGET_CHANNEL)
+    print("ready!")
 
 
+async def on_message(msg: ChatMessage):
+    to_say = msg.user.name + ": " + msg.text
+    speak_message(to_say)
+
+
+async def run():
+    twitch = await Twitch(APP_ID, APP_SECRET)
+    auth = UserAuthenticator(twitch, USER_SCOPE)
+    token, refresh_token = await auth.authenticate()
+    await twitch.set_user_authentication(token, USER_SCOPE, refresh_token)
+
+    chat = await Chat(twitch)
+
+    chat.register_event(ChatEvent.READY, on_ready)
+    chat.register_event(ChatEvent.MESSAGE, on_message)
+
+    chat.start()
+
+    try:
+        input('press ENTER to stop\\n')
+    finally:
+        chat.stop()
+        await twitch.close()
+
+
+asyncio.run(run())
