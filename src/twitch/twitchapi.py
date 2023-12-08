d5bbf8543d4f872f335ff6967fbfb9cb14a0fc30
kaoplo <kaoplo@pawz.hu>
2023-12-08 11:29:34 +0100

    asyncio event loop, we will see how this works out, currenty, on stopping, the loop will wait for one final message before stopping

--- src/twitch/twitchapi.py
+++ src/twitch/twitchapi.py
@@ -1,16 +1,24 @@
 import socket
 import random
 import re
+import threading
+import asyncio
+
+server = 'irc.chat.twitch.tv'
+port = 6667
+nickname = 'justinfan' + str(random.randint(100000, 999999))
+
 
 class Twitch:
     def __init__(self):
         self.sock = socket.socket()
         self.handlers = []
+        self.isRunning = False
 
     def register_event(self, handler):
         self.handlers.append(handler)
 
-    def notify(self, *args, **kwargs):
+    def notify_event(self, *args, **kwargs):
         for handler in self.handlers:
             handler(*args, **kwargs)
 
@@ -21,37 +29,50 @@ class Twitch:
             prefix, command, params, trailing = match.groups()
             return {
                 'username': prefix.split('!')[0] if prefix else None,
-                # 'command': command,
-                # 'params': params.split() if params else [],
                 'message': trailing
             }
         else:
             return None
 
     def connect(self, channel):
-        server = 'irc.chat.twitch.tv'
-        port = 6667
-        nickname = 'justinfan' + str(random.randint(100000, 999999))
+        self.isRunning = True
+        threading.Thread(target=self._open_socket, args=(channel,)).start()
+
+    def _open_socket(self, channel):
         self.sock.connect((server, port))
         self.sock.send(f"NICK {nickname}\n".encode('utf-8'))
         self.sock.send(f"JOIN #{channel}\n".encode('utf-8'))
         if "Welcome" not in self.sock.recv(2048).decode('utf-8'):
             raise RuntimeError("Failed to connect to Twitch IRC")
-        while True:
+        while self.isRunning:
             resp = self.sock.recv(2048).decode('utf-8')
             if resp.startswith('PING'):
                 self.sock.send("PONG\n".encode('utf-8'))
+            elif nickname in resp:
+                pass
             elif len(resp) > 0:
-                self.notify(self.parse_message(resp))
+                self.notify_event(self.parse_message(resp))
+
+    def close(self):
+        self.sock.close()
+        self.isRunning = False
 
 
 def on_message(message):
     print(f'{message["username"]} said: {message["message"]}')
 
-Twitch = Twitch()
 
-channel = 'kaoplo'
+async def run():
+    ttv = Twitch()
+    channel = 'kaoplo'
 
-Twitch.register_event(on_message)
+    ttv.connect(channel)
 
-Twitch.connect(channel)
+    ttv.register_event(on_message)
+
+    try:
+        input("press enter to stop\n")
+    finally:
+        ttv.close()
+
+asyncio.run(run())
