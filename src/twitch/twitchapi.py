c87f97dd7122eae4145f01749a8dafa41602368d
Ivan Sandor <kaoplo@pawz.hu>
2023-12-09 16:29:57 +0100

    Twitch.close() now closes correctly without erroring out, or waiting for another message

--- src/twitch/twitchapi.py
+++ src/twitch/twitchapi.py
@@ -3,6 +3,7 @@ import random
 import re
 import threading
 import asyncio
+import time
 
 server = 'irc.chat.twitch.tv'
 port = 6667
@@ -18,11 +19,19 @@ class Twitch:
     def register_event(self, handler):
         self.handlers.append(handler)
 
-    def notify_event(self, *args, **kwargs):
+    def connect(self, channel):
+        self.isRunning = True
+        threading.Thread(target=self._open_socket, args=(channel,)).start()
+
+    def close(self):
+        self.isRunning = False
+        self.sock.send("QUIT\n".encode('utf-8'))
+
+    def _notify_event(self, *args, **kwargs):
         for handler in self.handlers:
             handler(*args, **kwargs)
 
-    def parse_message(self, message):
+    def _parse_message(self, message):
         irc_message_pattern = re.compile(r'^(?::([^ ]+) )?([^ ]+)(?: ([^:]+) :(.+))?')
         match = irc_message_pattern.match(message)
         if match:
@@ -34,29 +43,26 @@ class Twitch:
         else:
             return None
 
-    def connect(self, channel):
-        self.isRunning = True
-        threading.Thread(target=self._open_socket, args=(channel,)).start()
-
     def _open_socket(self, channel):
         self.sock.connect((server, port))
         self.sock.send(f"NICK {nickname}\n".encode('utf-8'))
         self.sock.send(f"JOIN #{channel}\n".encode('utf-8'))
         if "Welcome" not in self.sock.recv(2048).decode('utf-8'):
             raise RuntimeError("Failed to connect to Twitch IRC")
-        while self.isRunning:
-            resp = self.sock.recv(2048).decode('utf-8')
-            if resp.startswith('PING'):
-                self.sock.send("PONG\n".encode('utf-8'))
-            elif nickname in resp:
-                pass
-            elif len(resp) > 0:
-                self.notify_event(self.parse_message(resp))
+        try:
+            while self.isRunning:
+                self._receive_message(self.sock.recv(2048).decode('utf-8'))
+        finally:
+            self.sock.close()
 
-    def close(self):
-        self.sock.close()
-        self.isRunning = False
-
+    def _receive_message(self, message):
+        resp = message
+        if resp.startswith('PING'):
+            self.sock.send("PONG\n".encode('utf-8'))
+        elif nickname in resp:
+            pass
+        elif len(resp) > 0:
+            self._notify_event(self._parse_message(resp))
 
 def on_message(message):
     print(f'{message["username"]} said: {message["message"]}')
